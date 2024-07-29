60f21441a6915fcaa4f83dae5c3700e895454b76
arod1104 <rodriguez.alex1828@gmail.com>
2024-07-29 00:07:06 -0500

    Added my solution to exercise 5 before implmenting fix

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+  if (args.length < 2) {
+    return "ERROR";
+  }
 
+  for (let i = 0; i < args.length; i++) {
+    if (
+      typeof args[i] !== "number" ||
+      args[i] < 0 ||
+      Math.floor(args[i]) !== args[i]
+    ) {
+      return "ERROR";
+    }
+  }
+
+  let result = 0;
+  for (let i = Math.min(...args); i <= Math.max(...args); i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
