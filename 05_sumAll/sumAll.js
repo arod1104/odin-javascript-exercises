f7225c60bf07afe30a20cfa88ebece74c18bb472
arod1104 <rodriguez.alex1828@gmail.com>
2024-07-29 00:08:14 -0500

    Added fix to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,14 @@
-const sumAll = function (...args) {
-  if (args.length < 2) {
-    return "ERROR";
-  }
-
-  for (let i = 0; i < args.length; i++) {
-    if (
-      typeof args[i] !== "number" ||
-      args[i] < 0 ||
-      Math.floor(args[i]) !== args[i]
-    ) {
-      return "ERROR";
-    }
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
   }
 
   let result = 0;
-  for (let i = Math.min(...args); i <= Math.max(...args); i++) {
+  for (let i = min; i <= max; i++) {
     result += i;
   }
   return result;
