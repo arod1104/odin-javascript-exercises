6a663c50aa93f473e40d6a971488b05ea1a0e334
arod1104 <rodriguez.alex1828@gmail.com>
2024-08-11 00:32:24 -0500

    Added my solution to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const arr = string.split("");
+  const regex = /[a-zA-Z0-9]/;
+  const filteredChars = arr.filter((char) => regex.test(char));
+  const cleanArr = filteredChars.map((char) => char.toLowerCase());
+  return cleanArr.join("") === cleanArr.reverse().join("");
 };
 
 // Do not edit below this line
