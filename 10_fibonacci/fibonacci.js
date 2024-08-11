1aa7545cb6fc6e6656d74d9193aef6a1a25bbeaf
arod1104 <rodriguez.alex1828@gmail.com>
2024-08-11 00:40:13 -0500

    Added my solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+  if (typeof index !== "number") {
+    index = parseInt(index);
+  }
+  if (index < 0) return "OOPS";
+  if (index === 0) return 0;
+  if (index === 1) return 1;
+  let fib = [0, 1];
+  for (let i = 2; i <= index; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib[index];
 };
 
 // Do not edit below this line
